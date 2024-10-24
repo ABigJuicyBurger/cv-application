cc75f52bdf797ad36fdefe0db1fc380b234789b5
ABigJuicyBurger <70484524+ABigJuicyBurger@users.noreply.github.com>
2024-10-24 10:35:33 -0600

    added general info input and labels

---
+++ src/GeneralInfo.jsx
@@ -0,0 +1,17 @@
+function GeneralInfo() {
+  return (
+    <div>
+      <h1>General Information</h1>
+      <section>
+        <label>Name</label>
+        <input></input>
+        <label>Email</label>
+        <input></input>
+        <label>Phone Number</label>
+        <input></input>
+      </section>
+    </div>
+  );
+}
+
+export default GeneralInfo;
