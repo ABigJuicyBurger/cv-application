440ba91ed1d3c654055693bedb08d4e00d8aa9f7
ABigJuicyBurger <70484524+ABigJuicyBurger@users.noreply.github.com>
2024-10-24 10:48:03 -0600

    added simple componenwith state on input fileds

--- src/GeneralInfo.jsx
+++ src/GeneralInfo.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
+
 function GeneralInfo() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function handlePhoneNumberChange(e) {
+    setPhoneNumber(e.target.value);
+  }
+
+  console.log("Current name value:", name);
+
   return (
     <div>
       <h1>General Information</h1>
       <section>
         <label>Name</label>
-        <input></input>
+        <input value={name} onChange={handleNameChange}></input>
         <label>Email</label>
-        <input></input>
+        <input value={email} onChange={handleEmailChange}></input>
         <label>Phone Number</label>
-        <input></input>
+        <input value={phoneNumber} onChange={handlePhoneNumberChange}></input>
       </section>
     </div>
   );
