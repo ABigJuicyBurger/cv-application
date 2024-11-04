cff51dbaf12ee64bc58fd6b68d2b491791124f11
ABigJuicyBurger <70484524+ABigJuicyBurger@users.noreply.github.com>
2024-11-04 09:48:00 -0700

    Configure for deployment using netlify

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <div className="header">
+      <h1>Resume Template Generator</h1>
+      <p>
+        Made by{" "}
+        <a href="https://github.com/ABigJuicyBurger/cv-application">
+          ABigJuicyBurger
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Header;
