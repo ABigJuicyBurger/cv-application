063fd6dc4f0a8287c935f501cab8010b548259a5
ABigJuicyBurger <70484524+ABigJuicyBurger@users.noreply.github.com>
2024-11-04 09:02:57 -0700

    added header with link to profile

--- src/ResumeTemplate.jsx
+++ src/ResumeTemplate.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Header from "./Header";
 import GeneralInfo from "./GeneralInfo";
 import Education from "./Education";
 import PracticalExperience from "./PracticalExperience";
@@ -10,22 +11,27 @@ function ResumeTemplate() {
   const toggleViewMode = () => setIsViewMode(!isViewMode);
 
   return (
-    <div className="resume-container">
-      <div className="controls-section">
-        <h1>Resume Template Generator</h1>
-        <button onClick={() => toggleViewMode()}>Toggle View/Edit</button>
+    <>
+      <div className="header">
+        <Header />
       </div>
-      <div className="resume-content">
-        <GeneralInfo isViewMode={isViewMode} toggleView={toggleViewMode} />
-        <Education isViewMode={isViewMode} toggleView={toggleViewMode} />
-        <PracticalExperience
-          isViewMode={isViewMode}
-          toggleView={toggleViewMode}
-        />
-        {/* Pass view and toggleView as props */}
-        {console.log("Current view value:", isViewMode)}
+      <div className="resume-container">
+        <div className="controls-section">
+          <h1>Resume Template Generator</h1>
+          <button onClick={() => toggleViewMode()}>Toggle View/Edit</button>
+        </div>
+        <div className="resume-content">
+          <GeneralInfo isViewMode={isViewMode} toggleView={toggleViewMode} />
+          <Education isViewMode={isViewMode} toggleView={toggleViewMode} />
+          <PracticalExperience
+            isViewMode={isViewMode}
+            toggleView={toggleViewMode}
+          />
+          {/* Pass view and toggleView as props */}
+          {console.log("Current view value:", isViewMode)}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
